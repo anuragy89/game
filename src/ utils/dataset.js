c07324ba315c3427fad986c19a7b10ab91868613
yuva neta <anuragyadavop89@gmail.com>
2026-02-08 16:09:37 +0530

    Create dataset.js

---
+++ src/ utils/dataset.js
@@ -0,0 +1,25 @@
+exports.truth = [
+  "🤔 What is your biggest secret?",
+  "❤️ Who do you secretly like?",
+  "😅 Most embarrassing moment?",
+  "🧠 Biggest fear?",
+  "🔥 Guilty pleasure?",
+  "👀 Who do you stalk the most?",
+  "💔 Ever broken a heart?",
+  "📱 Last chat you deleted?",
+  "🎭 Fake personality you show?",
+  "😈 One thing you regret?"
+];
+
+exports.dare = [
+  "😂 Send your most used emoji",
+  "📸 Send a random selfie",
+  "🎤 Send a voice note",
+  "👋 Tag someone and say hi",
+  "😎 Use emojis only for 3 messages",
+  "📝 Change your name for 5 mins",
+  "💬 Compliment someone",
+  "🕺 Send a dance emoji",
+  "📢 Say I love this group",
+  "🧠 Share a fun fact"
+];
